6ed1f98b06f47b882ebedec228ea4c39931f335d
Abhayrajsinh Gohil <53107480+mxxnwolf@users.noreply.github.com>
2021-12-16 01:23:27 +0530

    Update VoterList.sol

--- VoterList.sol
+++ VoterList.sol
@@ -1,20 +1,19 @@
-pragma solidity >=0.4.21 <0.9.0;
 contract VoterList is IUC{
-            userlist[] ListOfVoters;
-            address newIUC = IUC.new("name", 12, "male", userAddress, ECIAddress);
+    userlist[] ListOfVoters;
+    address newIUC = IUC.new("name", 12, "male", userAddress, eciMember);
 
-            function GetAllVoters() returns UserAddress {
-                for (var i = 0; i < ListOfVoters.length; i++)  {
-                    printf(ListOfVoters[i]);
-                }
-            }
-            function GetContractAddress(UserAddress) {
-                return newIUC(userAddress);
-            }
-            function GetUserDetails(ContractAddress) {
-                return _myData;
-            }
-            function IsRegistred(address _user)ownerOnly public {
-                voters[_user].IsRegistred = true;
-            }
+    function GetAllVoters() returns UserAddress {
+        for (var i = 0; i < ListOfVoters.length; i++)  {
+            printf(address.ListOfVoters[i]);
+        }
+    }
+    function GetContractAddress(UserAddress) {
+        return newIUC(userAddress);
+    }
+    function GetUserDetails(ContractAddress) {
+        return _myData;
+    }
+    function IsRegistred(address _user)ownerOnly public {
+        voters[_user].IsRegistred = true;
+    }
 }
